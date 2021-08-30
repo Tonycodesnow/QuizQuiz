3e4db569e1e310806deecdaf95cbdabdb5838d38
Ant <huertinez.family@outlook.com>
2021-08-29 23:40:17 -0700

    completed readme and MVP quiz challenge

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# QuizQuiz
-challenge 4
+# A simple timed quiz that is supposed to track the score of the player
+
+* Timer starts from 75 sec
+* When user clicks A question and 4 choices will appear
+* When a question is answered user will be presented with another question
+* user will get penalized -10 sec if question is answered incorrectly
+* When all questions are answered or timer is reduced to 0 It's game over
+* user can input initials 
+
+
+<p align="center"><img src="./assets/images/pix.png" width="70%"></p>
+
+
+
+- side note This assignment was very challenging and although I made many mistakes with my attempts to complete this taks I can surely say I learned a lot throughout this challenge. 
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,7 +3,7 @@ var scoreText = document.querySelector("#scorelink");
 var scoreBtn = document.getElementById("scorebtn");
 var acceptAnswers = true
 var questionIndex = 0
-var score = JSON.parse(localStorage.getItem("quiz-scores"))||[];
+var score = JSON.parse(localStorage.getItem("quiz-scores")) || [];
 
 var endPage = document.getElementById('endpage')
 //  cerated HTML elements  
@@ -19,38 +19,38 @@ var timePenalty = 10
 
 // questions section
 var questions = [{
-    question: 'Which of the following is NOT an advantage of Moment.js?',
-    choice: ['Parsing dates is easier.', 'Setting timers is easier', 'Formatting dates is easier.', 'Manipulating dates (adding or subtracting days) is easier.'],
-    answer: 'Parsing dates is easier.'
-},
-{
-    question: 'What is (9 x 10)?',
-    choice: ['19', '21', '90', '43'],
-    answer: '90'
-},
-{
-    question: 'How often does the moon orbit the earth?',
-    choice: ['30 days', 'Every 24 hours', 'No orbit the Earth is flat', '27 days'],
-    answer: '27 days'
-    
-},
-{
-    question: 'How many languages are written from right to Left?',
-    choice: ['12', '15', '51', 'I dont understand this question.'],
-    answer: '12'
-},
-{
-    question: 'How often does a person touch thier face every hour?',
-    choice: ['3', '7', '10', '16'],
-    answer: '10'
-},
+        question: 'Which of the following is NOT an advantage of Moment.js?',
+        choice: ['Parsing dates is easier.', 'Setting timers is easier', 'Formatting dates is easier.', 'Manipulating dates (adding or subtracting days) is easier.'],
+        answer: 'Parsing dates is easier.'
+    },
+    {
+        question: 'What is (9 x 10)?',
+        choice: ['19', '21', '90', '43'],
+        answer: '90'
+    },
+    {
+        question: 'How often does the moon orbit the earth?',
+        choice: ['30 days', 'Every 24 hours', 'No orbit the Earth is flat', '27 days'],
+        answer: '27 days'
+
+    },
+    {
+        question: 'How many languages are written from right to Left?',
+        choice: ['12', '15', '51', 'I dont understand this question.'],
+        answer: '12'
+    },
+    {
+        question: 'How often does a person touch thier face every hour?',
+        choice: ['3', '7', '10', '16'],
+        answer: '10'
+    },
 ];
 
 // swhat happens when the button starts
 startQuiz.addEventListener('click', () => {
+    countdown();
     hideIntro();
     startGame();
-    countdown();
 });
 
 // hiding the landing page section
@@ -66,7 +66,7 @@ function hideIntro() {
 // showing the hidden questions section
 function startGame() {
     quizEl.setAttribute('style', 'display: block');
-    
+
     nextQuestion();
 }
 
@@ -75,28 +75,28 @@ function nextQuestion() {
     // display question
     var current = questions[questionIndex];
     var question = document.querySelector("#question");
-    
+
     var choice1 = document.getElementById('choice1');
     var choice2 = document.getElementById('choice2');
     var choice3 = document.getElementById('choice3');
     var choice4 = document.getElementById('choice4');
-    
+
     // adding each item in the array of choice to each of the choices 
     question.textContent = current.question;
-    
+
     choice1.textContent = current.choice[0]
     choice2.textContent = current.choice[1]
     choice3.textContent = current.choice[2]
     choice4.textContent = current.choice[3]
-    
+
     document.querySelectorAll('.option').forEach(function (choiceEl) {
         choiceEl.addEventListener('click', checkChoice)
     })
 }
 
-function checkChoice (event) {
-    var choice; 
-    if(event.target.children.length=== 0){
+function checkChoice(event) {
+    var choice;
+    if (event.target.children.length === 0) {
         choice = event.target.textContent
     } else {
         choice = event.target.children[0].textContent
@@ -109,8 +109,8 @@ function checkChoice (event) {
         // timeLeft= timeLeft - timePenalty 
         timerEl.textContent = "" + timeLeft;
     }
-    questionIndex = questionIndex +1
-    if (questionIndex < questions.length){
+    questionIndex = questionIndex + 1
+    if (questionIndex < questions.length) {
         nextQuestion()
     } else {
         scoreScreen()
@@ -120,17 +120,17 @@ function checkChoice (event) {
 // countdown timer section
 function countdown() {
     timeLeft = 75;
-    
+
     var timeInterval = setInterval(function () {
-        
+
         if (timeLeft > 1) {
-            
+
             timerEl.textContent = "" + timeLeft;
-            
+
             timeLeft--;
         } else {
             scoreScreen()
-            
+
         }
     }, 1000);
 }
@@ -139,7 +139,7 @@ function countdown() {
 // checking answer section 
 var scoreScreen = function () {
     timerEl.textContent = '';
-    
+
     endPage.removeAttribute("hidden")
     clearInterval(counter);
     // clearInterval(timeInterval);
@@ -147,7 +147,7 @@ var scoreScreen = function () {
 
 // endPage.setAttribute('style', 'display: block');
 
-var storage = function(event) {
+var storage = function (event) {
     event.preventDefault()
     var userInput = document.getElementById('scoreInput').value
     var scores = {
@@ -160,6 +160,7 @@ var storage = function(event) {
 }
 
 scoreBtn.addEventListener("click", storage)
+
 function scorepage() {
     location.href = "scores.html"
 }
